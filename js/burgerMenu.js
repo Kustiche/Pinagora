e014b276a2fa2dc865e06780014f44f3934876de
Kustiche <ChernichnyKustik@mail.ru>
2026-01-28 16:12:06 +0300

    Доработал бургер меню

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,9 +1,18 @@
 const burgerBtn = document.querySelector('.burger');
 const burgerNav = document.querySelector('.burger-nav');
 const body = document.querySelector('body');
+const navLinks = document.querySelectorAll('.burger-nav__link');
 
 burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('open');
   burgerNav.classList.toggle('open');
   body.classList.toggle('no-scroll');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    burgerBtn.classList.remove('open');
+    burgerNav.classList.remove('open');
+    body.classList.remove('no-scroll');
+  });
 });
